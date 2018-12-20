c66e6194086eca84bc730d7676f9f17b60fdcddc
Will Smythe <wismythe@microsoft.com>
2018-12-20 16:10:43 -0500

    Change cache directory

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -9,4 +9,5 @@ module.exports = Object.assign({}, require('./jest.config'), {
     ['jest-junit', {output: 'reports/junit/js-test-results.xml'}],
     ['jest-silent-reporter', {useDots: true}],
   ],
+  cacheDirectory: '/extra/jest-cache'
 });
